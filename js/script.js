dd0d70ef5dd6276f3833ffd0301be94de0418d37
“jipetilly” <jipetilly@gmail.com>
2021-01-26 14:08:21 +0100

    Mardi 14h08

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@ const overblack = document.querySelector('.overblack')
 window.addEventListener('load', ()=> {
     overblack.classList.add ('overblackOut')
 })
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  })
