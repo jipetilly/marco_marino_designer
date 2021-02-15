257b2d855455bd49a636bb514d6f6d37821a3d35
jipetilly <jipetilly@gmail.com>
2021-02-15 08:35:52 +0100

    version lundi 15-02

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-// const overblack = document.querySelector('.overblack')
-// window.addEventListener('load', ()=> {
-//     overblack.classList.add ('overblackOut')
-// })
+//SWIPER JS 
 
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
@@ -20,3 +17,44 @@ var mySwiper = new Swiper('.swiper-container', {
     },
   
   })
+
+
+  
+//contact closed button
+let contactLink = document.querySelector('.contactLink');
+let contactForm = document.querySelector('.contact');
+let closeBtn = document.querySelector('.close');
+
+contactLink.addEventListener('click', function(){
+    contactForm.classList.add('closed');
+});
+
+closeBtn.addEventListener('click', function(){
+    contactForm.classList.remove('closed');
+});
+
+
+// SCROLL REVEAL CONTENU 
+let hWindow = window.innerHeight;
+console.log(hWindow)
+
+let imgReveal = document.querySelectorAll('.reveal');
+console.log(imgReveal)
+
+
+window.addEventListener('scroll', function(){
+    let leScroll = window.pageYOffset;
+
+    for (let i=0; i<imgReveal.length; i++) {
+        let leTopImg = imgReveal[i].getBoundingClientRect();
+        console.log (leTopImg)
+    
+        let hImg = imgReveal[i].offsetHeight;
+        console.log (hImg);
+            if (leScroll > (leTopImg.y + (hImg / 5))) {
+                imgReveal[i].classList.add('revealed');
+            } else if (leScroll < (leTopImg.y + (hImg / 10))) {
+                imgReveal[i].classList.remove('revealed');
+            }
+    }
+})
